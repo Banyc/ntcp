7e7d092ff72148979007adfc5ce305981323903f
Banyc <36535895+Banyc@users.noreply.github.com>
2023-02-09 11:32:35 +0800

    feat: SendQueue

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,5 +1,7 @@
 mod receive_queue;
 mod scheduler;
+mod send_queue;
 
 pub use receive_queue::*;
 pub use scheduler::*;
+pub use send_queue::*;
---
+++ src/core/send_queue.rs
@@ -0,0 +1,83 @@
+use std::collections::BTreeSet;
+
+use seq::Seq16;
+
+pub struct SendQueue {
+    /// The queue of sending packets
+    queue: BTreeSet<Seq16>,
+    /// The maximum number of packets that can be stored in the queue
+    capacity: usize,
+    /// The sequence number of the next new packet
+    shadow_end: Seq16,
+}
+
+impl SendQueue {
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            queue: BTreeSet::new(),
+            capacity,
+            shadow_end: Seq16::new(0),
+        }
+    }
+
+    pub fn send(&mut self) -> Option<Seq16> {
+        // Reject if the queue is full
+        if self.queue.len() >= self.capacity {
+            return None;
+        }
+
+        // Insert the new packet
+        let seq = self.shadow_end;
+        self.queue.insert(seq);
+
+        // Increment the shadow end
+        self.shadow_end = seq.add(1);
+
+        Some(seq)
+    }
+
+    pub fn ack(&mut self, seq: Seq16) {
+        self.queue.remove(&seq);
+    }
+
+    pub fn set_capacity(&mut self, capacity: usize) {
+        self.capacity = capacity;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ok() {
+        let mut queue = SendQueue::new(2);
+        assert_eq!(queue.send(), Some(Seq16::new(0)));
+        assert_eq!(queue.send(), Some(Seq16::new(1)));
+        assert_eq!(queue.send(), None);
+        queue.ack(Seq16::new(0));
+        assert_eq!(queue.send(), Some(Seq16::new(2)));
+        assert_eq!(queue.send(), None);
+        queue.ack(Seq16::new(2));
+        assert_eq!(queue.send(), Some(Seq16::new(3)));
+        assert_eq!(queue.send(), None);
+        queue.ack(Seq16::new(1));
+        assert_eq!(queue.send(), Some(Seq16::new(4)));
+        assert_eq!(queue.send(), None);
+    }
+
+    #[test]
+    fn reset_capacity() {
+        let mut queue = SendQueue::new(2);
+        assert_eq!(queue.send(), Some(Seq16::new(0)));
+        assert_eq!(queue.send(), Some(Seq16::new(1)));
+        assert_eq!(queue.send(), None);
+        queue.set_capacity(1);
+        assert_eq!(queue.send(), None);
+        queue.ack(Seq16::new(0));
+        assert_eq!(queue.send(), None);
+        queue.ack(Seq16::new(1));
+        assert_eq!(queue.send(), Some(Seq16::new(2)));
+        assert_eq!(queue.send(), None);
+    }
+}
