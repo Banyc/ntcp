42d84e1745fd0631f455dafaf59429eb8d494fa0
Banyc <36535895+Banyc@users.noreply.github.com>
2023-02-10 13:36:53 +0800

    refactor: merge RetransmitQueue and TimedSendQueue

--- src/core/send/mod.rs
+++ src/core/send/mod.rs
@@ -1,9 +1,10 @@
-mod retransmit_queue;
 mod rtt_stopwatch;
 mod scheduler;
 mod send_queue;
+mod timed_send_queue;
 
-pub use retransmit_queue::*;
+
 pub use rtt_stopwatch::*;
 pub use scheduler::*;
 pub use send_queue::*;
+pub use timed_send_queue::*;
--- src/core/send/timed_send_queue.rs
+++ src/core/send/timed_send_queue.rs
@@ -1,11 +1,11 @@
-use std::{collections::HashMap, time};
+use std::{collections::BTreeMap, time};
 
 use seq::Seq16;
 
 use super::{RttStopwatch, SendQueue};
 
 pub struct TimedSendQueue {
-    rtt_stopwatches: HashMap<Seq16, RttStopwatch>,
+    rtt_stopwatches: BTreeMap<Seq16, RttStopwatch2>,
     /// Packets that have been sent but not yet acknowledged
     send_queue: SendQueue,
 }
@@ -14,31 +14,125 @@ impl TimedSendQueue {
     #[must_use]
     pub fn new(capacity: usize) -> Self {
         Self {
-            rtt_stopwatches: HashMap::new(),
+            rtt_stopwatches: BTreeMap::new(),
             send_queue: SendQueue::new(capacity),
         }
     }
 
-    pub fn cancel_rtt_stopwatch(&mut self, seq: Seq16) {
-        self.rtt_stopwatches.remove(&seq);
+    pub fn retransmit(
+        &mut self,
+        seq: Seq16,
+        now: time::Instant,
+        timeout: time::Duration,
+    ) -> Result<(), RetransmitError> {
+        // Cancel the rtt stopwatch
+        let old_stopwatch = self.rtt_stopwatches.remove(&seq);
+        if old_stopwatch.is_none() {
+            return Err(RetransmitError::SequenceNumberNotFound);
+        }
+
+        // Start a new rtt stopwatch
+        self.rtt_stopwatches.insert(
+            seq,
+            RttStopwatch2 {
+                stopwatch: RttStopwatch::new(now, timeout),
+                invalidate_rtt: true,
+            },
+        );
+
+        Ok(())
     }
 
     pub fn rtt_stopwatch(&self, seq: Seq16) -> Option<&RttStopwatch> {
-        self.rtt_stopwatches.get(&seq)
+        self.rtt_stopwatches
+            .get(&seq)
+            .map(|stopwatch| &stopwatch.stopwatch)
+    }
+
+    pub fn collect_timeout_sequences(&self, now: time::Instant) -> Vec<Seq16> {
+        // Collect all timed out sequences
+        let mut sequences = Vec::new();
+        for (seq, rtt_stopwatch) in &self.rtt_stopwatches {
+            if rtt_stopwatch.stopwatch.has_timed_out(now) {
+                sequences.push(*seq);
+            }
+        }
+
+        sequences
     }
 
     pub fn send(&mut self, now: time::Instant, timeout: time::Duration) -> Option<Seq16> {
         let Some(seq) = self.send_queue.send() else {
             return None;
         };
-        self.rtt_stopwatches
-            .insert(seq, RttStopwatch::new(now, timeout));
+        self.rtt_stopwatches.insert(
+            seq,
+            RttStopwatch2 {
+                stopwatch: RttStopwatch::new(now, timeout),
+                invalidate_rtt: false,
+            },
+        );
         Some(seq)
     }
 
     pub fn ack(&mut self, seq: Seq16, now: time::Instant) -> Option<time::Duration> {
         self.send_queue.ack(seq);
-        let rtt_stopwatch = self.rtt_stopwatches.remove(&seq);
-        rtt_stopwatch.map(|stopwatch| stopwatch.into_rtt(now))
+        let Some(rtt_stopwatch) = self.rtt_stopwatches.remove(&seq) else {
+            return None;
+        };
+        match rtt_stopwatch.invalidate_rtt {
+            true => None,
+            false => Some(rtt_stopwatch.stopwatch.into_rtt(now)),
+        }
+    }
+}
+
+struct RttStopwatch2 {
+    stopwatch: RttStopwatch,
+    invalidate_rtt: bool,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub enum RetransmitError {
+    SequenceNumberNotFound,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ok() {
+        let mut queue = TimedSendQueue::new(10);
+        let now = time::Instant::now();
+        let timeout = time::Duration::from_millis(100);
+        assert_eq!(
+            queue.retransmit(Seq16::new(0), now, timeout),
+            Err(RetransmitError::SequenceNumberNotFound)
+        );
+        assert_eq!(queue.send(now, timeout), Some(Seq16::new(0)));
+        assert_eq!(queue.retransmit(Seq16::new(0), now, timeout), Ok(()));
+        assert_eq!(
+            queue.retransmit(Seq16::new(1), now, timeout),
+            Err(RetransmitError::SequenceNumberNotFound)
+        );
+        let now = now + timeout;
+        assert_eq!(queue.collect_timeout_sequences(now), vec![Seq16::new(0)]);
+        assert_eq!(queue.ack(Seq16::new(0), now), None);
+        assert_eq!(
+            queue.retransmit(Seq16::new(0), now, timeout),
+            Err(RetransmitError::SequenceNumberNotFound)
+        );
+    }
+
+    #[test]
+    fn rtt() {
+        let mut queue = TimedSendQueue::new(10);
+        let now = time::Instant::now();
+        let timeout = time::Duration::from_millis(100);
+        assert_eq!(queue.send(now, timeout), Some(Seq16::new(0)));
+        let rtt = time::Duration::from_millis(50);
+        let now = now + rtt;
+        assert_eq!(queue.ack(Seq16::new(0), now), Some(rtt));
     }
 }
